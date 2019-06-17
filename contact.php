477075ccfc259368c0b1d58ba0154198ecb20d9b
Berk Cetinsaya <berkcetinsaya@gmail.com>
2019-06-17 16:19:15 -0500

    Header and footer files created

--- contact.php
+++ contact.php
@@ -1,49 +1,7 @@
-<!DOCTYPE html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Task Management System For Overseas Engineers">
-        <meta name="author" content="Berk Cetinsaya">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/css/modern-business.css" rel="stylesheet">
-        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <script src="assets/js/jquery.min.js"></script>
-        <title>
-            Task Management System For Overseas Engineers
-        </title>
-    </head>
-    <!-- Navigation -->
-    <nav style="background-color:#800000;" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Task Management System For Overseas Engineers</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="task.php">Tasks</a>
-                    </li>
-                    <li>
-                        <a href="user.php">Users</a>
-                    </li>
-                    <li>
-                        <a href="contact.php">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<?php
+$page_title = "Contact";
+include_once "header.php";
+?>
 
     <!-- Page Content -->
     <div class="container">
@@ -93,23 +51,5 @@
             </div>
         </div>
         <!-- /.row -->
-        <hr>
-        <!-- Footer -->
-        <footer>
-            <div class="row text-center">
-                <div class="col-lg-12">
-                    <p>Copyright <b>Berk Cetinsaya</b> &copy; <?php echo date("Y"); ?></p>
-                </div>
-            </div>
-        </footer>
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="assets/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="assets/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+        
+        <?php include_once("footer.php");?>
--- user.php
+++ user.php
@@ -8,54 +8,11 @@
 		$type           = $row['type'];	
 	}
 	$result->free();
+	
+	$page_title = "Users";
+	include_once "header.php";
+
 ?>
-<!DOCTYPE HTML>
-	<head>
-		<meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Task Management System For Overseas Engineers">
-        <meta name="author" content="Berk Cetinsaya">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/css/modern-business.css" rel="stylesheet">
-        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <script src="assets/js/jquery.min.js"></script>
-        <title>
-            TMSFOE | Task Management System For Overseas Engineers
-        </title>
-	</head>
-    <body>
-		<!-- Navigation -->
-		<nav style="background-color:#800000;" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="index.php">TMSFOE</a>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="task.php">Tasks</a>
-						</li>
-						<li>
-							<a href="user.php">Users</a>
-						</li>
-						<li>
-							<a href="contact.php">Contact</a>
-						</li>
-					</ul>
-				</div>
-				<!-- /.navbar-collapse -->
-			</div>
-			<!-- /.container -->
-		</nav>
 		<!-- Page Content -->
 		<div class="container">
 			<!-- Page Heading/Breadcrumbs -->
@@ -274,16 +231,4 @@
 					
 				</div>
 			</div>
-			<hr>
-			<footer>
-				<div class="row text-center">
-					<div class="col-lg-12">
-						<p>Copyright <b>Berk Cetinsaya</b> &copy; <?php echo date("Y"); ?></p>
-					</div>
-				</div>
-			</footer>			
-		</div>
-		<script src="assets/js/jquery.js"></script>
-    	<script src="assets/js/bootstrap.min.js"></script>
-    </body>
-</html>
+        <?php include_once("footer.php");?>
